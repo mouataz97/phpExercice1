b92f4dae5474e266665a6998b5241061d0a79e8e
PHP course <mouatazbillah97@gmail.com>
2025-01-25 12:20:42 +0100

    creating function to read the file

--- app/App.php
+++ app/App.php
@@ -3,3 +3,14 @@
 declare(strict_types = 1);
 
 // Your Code
+function getfiletransaction(): array
+{
+    $files = [];
+    foreach(scandir(FILES_PATH) as $file){
+        if(is_dir($file)){
+            continue;
+        }
+        $files[] = $file;
+    }
+    return $files;
+}
